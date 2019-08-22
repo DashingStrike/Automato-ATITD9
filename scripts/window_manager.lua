4fa3a91407efee107004a66ed40b4f04bba0fd29
Ben Alderson <b.alderson@gmx.com>
2019-08-22 11:08:44 +0100

    Spacing fix

--- scripts/window_manager.lua
+++ scripts/window_manager.lua
@@ -2,5 +2,5 @@ dofile("common.inc");
 
 function doit()
   askForWindow("Window Manager v1.0 by Tallow");
-  windowManager("Window Manager", nil, true, true);
+  windowManager("Window Manager", nil, true, true, nil, nil, nil, 8, 13);
 end
