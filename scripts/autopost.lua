6a7a315c3fa1200dcdfa1ad16a282757e623b29c
ldobson <lucas.dobson@gmail.com>
2019-10-19 21:13:42 -0400

    Remove debug, fix ask text

--- scripts/autopost.lua
+++ scripts/autopost.lua
@@ -1,15 +1,14 @@
 dofile("common.inc");
 
-initialText = "Censorship-free autoposting macro. Automatically passes through messages. Hover over ATITD, press shift.";
+local askText = "Censorship-free autoposting macro.\n\nAutomatically passes through messages.";
 
 function doit()
-   askForWindow(printText);
+   askForWindow(askText);
    while 1 do
       srReadScreen();
       local passicon = srFindImage("cb-pass.png");
       if passicon then
          safeClick(passicon[0] + 1, passicon[1] + 1);
-         lsPrintln(string.format("Clicking %s, %s", passicon[0] + 1, passicon[1] + 1));
       end
       sleepWithStatus(500, "Waiting for new message");
    end
