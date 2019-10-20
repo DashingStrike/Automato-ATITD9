4f4121bb98a453153d5c5de2a85f281749fd81f8
Cegaiel <sharpguy32@yahoo.com>
2019-10-20 09:14:47 -0400

    test_findImage_offset.lua - Find all images and point to them all. Previously it would only detect first image only.  Improved GUI.

--- scripts/test_findImage_offset.lua
+++ scripts/test_findImage_offset.lua
@@ -44,8 +44,7 @@ function mainMenu()
         end
   lsSetCamera(0,0,lsScreenX*1.0,lsScreenY*1.0);  -- Shrink the text boxes and text down
   y = y + 30;
-    if lsButtonText(10, lsScreenY - 30, z, 100, 0xFFFFFFff, "Next") then
-	lsDoFrame();
+    if lsButtonText(10, lsScreenY - 30, z, 100, 0xFFFFFFff, "Search") then
       findPNG();
     end
 
@@ -63,24 +62,15 @@ function findPNG()
     srReadScreen();
 
   if image ~= "" then
-    findBlah = srFindImage(image);
+    findBlah = findAllImages(image);
   end
 
   if not findBlah then
-    result = image .. " NOT Found";
-      if lsButtonText(10, lsScreenY - 30, z, 100, 0xFFFFFFff, "Go Back") then
-	  lsDoFrame();
-        mainMenu();
-      end
+    result = "NOT Found";
   else
-    result = image .. " FOUND\n\nClick Point button to put mouse on image";
-      if lsButtonText(10, lsScreenY - 30, z, 100, 0xFFFFFFff, "Point") then
-        pointToLocation();
-      end
+    result = "FOUND " .. #findBlah .. " Locations\n\nClick Point button to hover mouse over Locations";
   end
-
-  statusScreen(result, nil, 0.7);
-  lsSleep(10);
+  sleepWithStatus(100, result, nil, 0.7, "Searching for " .. image);
   end
 end
 
@@ -88,6 +78,7 @@ end
 function pointToLocation()
   local y = 20;
     while 1 do
+      disablePointButton = 1;
       if lsMouseIsDown(1) then
 	  lsSleep(50);
 	  -- Don't move mouse until we let go of mouse button
@@ -99,19 +90,30 @@ function pointToLocation()
 	    if not tonumber(yOffset) then
 	      yOffset = 0;
 	    end
-		srSetMousePos(findBlah[0]+xOffset,findBlah[1]+yOffset);
-		sleepWithStatus(6000, "Pointing to image: " .. image .. "\n\nX Offset: " 
-		.. xOffset .. "\nY Offset: " .. yOffset .. "\n\nMouse Location (no offset):\n" .. math.floor(findBlah[0]) .. ", " .. math.floor(findBlah[1]) .. "\n\nMouse Location (after offsets):\n" .. math.floor(findBlah[0]+xOffset) .. ", " .. 
-		math.floor(findBlah[1]+yOffset), nil, 0.7);
+
+		for i=1,#findBlah do
+		  srSetMousePos(findBlah[i][0]+xOffset,findBlah[i][1]+yOffset);
+		  sleepWithStatus(6000, "Pointing to Location: " .. i .. "/" .. #findBlah .. "\n\nX Offset: " 
+		  .. xOffset .. "\nY Offset: " .. yOffset .. "\n\nMouse Location (no offset):\n" .. math.floor(findBlah[i][0]) .. ", " .. math.floor(findBlah[i][1]) .. "\n\nMouse Location (after offsets):\n" .. math.floor(findBlah[i][0]+xOffset) .. ", " .. 
+		  math.floor(findBlah[i][1]+yOffset), nil, 0.7);
+		end
+
+      disablePointButton = nil;
+      break;
       end
-    end
+    end -- while
 end
 
 
 local waitChars = {"-", "\\", "|", "/"};
 local waitFrame = 1;
 
-function sleepWithStatus(delay_time, message, color, scale)
+function sleepWithStatus(delay_time, message, color, scale, waitMessage)
+  if not waitMessage then
+    waitMessage = "Waiting ";
+  else
+    waitMessage = waitMessage .. " ";
+  end
   if not color then
     color = 0xffffffff;
   end
@@ -125,18 +127,24 @@ function sleepWithStatus(delay_time, message, color, scale)
   while delay_time > (lsGetTimer() - start_time) do
     local frame = math.floor(waitFrame/5) % #waitChars + 1;
     time_left = delay_time - (lsGetTimer() - start_time);
-    local waitMessage = "Waiting ";
+    newWaitMessage = waitMessage;
     if delay_time >= 1000 then
-      waitMessage = waitMessage .. time_left .. " ms ";
+      newWaitMessage = waitMessage .. time_left .. " ms ";
     end
     lsPrintWrapped(10, 50, 0, lsScreenX - 20, scale, scale, 0xd0d0d0ff,
-		   waitMessage .. waitChars[frame]);
+                   newWaitMessage .. waitChars[frame]);
     statusScreen(message, color, nil, scale);
     lsSleep(tick_delay);
     waitFrame = waitFrame + 1;
 
-    if lsButtonText(10, lsScreenY - 30, z, 100, 0xFFFFFFff, "Go Back") then
+    if lsButtonText(lsScreenX - 110, lsScreenY - 60, z, 100, 0xFFFFFFff, "Go Back") then
       mainMenu();
+    end
+
+    if findBlah and not disablePointButton then
+      if lsButtonText(10, lsScreenY - 30, z, 100, 0xFFFFFFff, "Point") then
+        pointToLocation();
+      end
     end
   end
 end
