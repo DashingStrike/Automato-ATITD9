3a99b1cf25e4c2f950b98f76c248f90e96574181
Cegaiel <sharpguy32@yahoo.com>
2019-10-22 20:15:55 -0400

    Update test_ocr_text.lua

--- scripts/test_ocr_text.lua
+++ scripts/test_ocr_text.lua
@@ -1,7 +1,7 @@
 dofile("common.inc");
 
-xOffset = 0;
-yOffset = 0;
+xOffset = 10;
+yOffset = 2;
 pointingSpeed = 2000; --ms
 
 function doit()
@@ -125,6 +125,9 @@ function findStuff()
     lsPrint(10, lsScreenY - 30 - 18*2, 20, scale, scale, 0xFFFFFFff, "Click a string above to move mouse to that location.");
     lsPrint(10, lsScreenY - 30 - 18, 20, scale, scale, 0xFFFFFFff, "Click Point to move mouse to location(s).");
     if lsButtonText(10, lsScreenY - 30, 20, 100, 0xFFFFFFff, "Point") then
+      while lsMouseIsDown() do
+        sleepWithStatus(16, "Release Mouse to continue ...", nil, 0.7, "Preparing to Click");
+      end
       pointToLocation();
     end
   end
